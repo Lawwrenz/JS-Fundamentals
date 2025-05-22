3f973c9e287ecb853c19f420ec6ec532787f776b
Lawrence Ezealor <lawrenceezealor1@gmail.com>
2025-05-22 15:13:05 +0100

    5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 const num = process.argv[2];
 
 if (num === undefined || isNaN(num) || num.trim() === ''){
-    console.log("Not a Number");
+    console.log('Not a number');
 }else {
     console.log(`My number: ${parseInt(num, 10)}`);
 }
