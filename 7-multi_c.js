8f95a9dd783eb9949932f903152080ab79dda4a3
Lawrence Ezealor <lawrenceezealor1@gmail.com>
2025-05-22 17:20:00 +0100

    7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,6 +4,4 @@ if (!isNaN(x) && x > 0) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else if (process.argv[2] !== undefined) {
-  console.log('Missing number of occurrences');
 }
